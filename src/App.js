f003b5b9ccf1f9af3844eab85b75adad2c417a82
legloc <developer.aliev@gmail.com>
2020-12-19 20:47:45 +0300

    Created simple request to geo.ipify.org

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useState } from 'react'
+import axios from 'axios'
 import Form from './components/Form'
 import Info from './components/Info'
 import Map from './components/Map'
 import './App.sass'
 
 const App = () => {
+  const [loading, setLoading] = useState(false)
+  
+  const handleFormSubmit = ip => {
+    console.log('REQUEST STARTED')
+    setLoading(true)
+
+    axios.get(`https://geo.ipify.org/api/v1?apiKey=${ process.env.REACT_APP_IPIFY_API_KEY }&ipAddress=${ ip }`)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      .then(() => {
+        console.log('REQUEST FINISHED')
+        setLoading(false)
+      })
+  }
+
   return (
     <div className="wrapper">
       <header>
         <h2 className="header-title">IP Address Tracker</h2>
-        <Form />
-        <Info />
+        <Form handleSubmit={ ip => handleFormSubmit(ip) } />
+        <Info loading={ loading } />
       </header>
       <Map />
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import IconArrow from '../images/icon-arrow.svg'
 
-const Form = () => {
+const Form = ({ handleSubmit }) => {
+  const [state, setState] = useState('')
+
   return (
     <div className="form-group">
-      <input className="form-input" type="text" placeholder="Search for any IP address or domain" />
-      <button className="form-btn">
+      <input className="form-input" type="text" placeholder="Search for any IP address or domain" onChange={ e => setState(e.target.value) } value={ state } />
+      <button className="form-btn" onClick={ () => handleSubmit(state) }>
         <img src={ IconArrow } alt="Search" />
       </button>
     </div>
